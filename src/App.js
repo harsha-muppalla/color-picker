9b27e575f5099c6040766d7f0348909381ab5dde
harsha-muppalla <harshamuppalla4@gmail.com>
2023-10-16 00:02:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ColorPicker from './components/ColorPicker';
+function App() {
+  return (
+    <div>
+      <ColorPicker/>
+    </div>
+  );
+}
+
+export default App;
